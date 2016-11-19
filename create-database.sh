89c1466c07e919dbe7d4e46da851606d4727f071
John Blakkan <blakkan@users.noreply.github.com>
2016-11-19 22:59:46 +0000

    REALLY removed the unused .sql files

--- create-database.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/bash
-psql -U postgres -c 'CREATE DATABASE "Tcount" owner postgres;'
-psql -U postgres -d Tcount -c 'CREATE TABLE "Tweetwordcount" ( word VARCHAR UNIQUE primary key, count INT DEFAULT 0);'
