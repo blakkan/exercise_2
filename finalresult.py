1b2cd179a4494669673bffa8107c6fd49b225c50
John Blakkan <blakkan@users.noreply.github.com>
2016-11-20 00:00:24 +0000

    Version of finalresult.py and test code for it now running

---
+++ finalresult.py
@@ -0,0 +1,32 @@
+import sys
+import re
+import psycopg2
+
+
+# Do some very simple command-line argument validation
+if len(sys.argv) != 2:
+    print "You must call this program with exactly one word as a command line argument"
+    exit()
+
+the_word = sys.argv[1]
+
+# Do just a little more, just to prevent somebody from easily doing any sql injection...
+new_word = re.sub(r'[\;\"\,\)\(]', '', the_word)
+
+if the_word != new_word:
+    print "I won't let you try that word.  I would be willing to let you try %s" % new_word
+    exit()
+
+try:
+  conn = psycopg2.connect(database="Tcount", user="postgres", password="pass", host="localhost", port="5432")
+except:
+  print "Could not connect to the database.  I blame society."
+
+cur = conn.cursor()
+
+#could use "like" instead of = here, if we wanted to permit sql regex matching...
+sql_command = '''SELECT word, count FROM "Tweetwordcount" WHERE word = '%s'; ''' % new_word
+
+cur.execute(sql_command)
+records = cur.fetchall()
+print '''Total number of occurences of "%s": %d''' % ( new_word, len(records) )
--- for-use-by-bolt.py
+++ for-use-by-bolt.py
@@ -7,7 +7,10 @@
 
 import psycopg2
 
-conn = psycopg2.connect(database="tcount", user="postgres", password="pass", host="localhost", port="5432")
+try:
+  conn = psycopg2.connect(database="Tcount", user="postgres", password="pass", host="localhost", port="5432")
+except:
+  print "Could not connect to the database.  I blame society."
 
 cur = conn.cursor()
 
@@ -15,18 +18,19 @@ cur = conn.cursor()
 # upserts
 #
 
-def increment( my_word ):
-    cur.execute('''UPDATE Tweetwordcount SET count=count+1 WHERE word = '{}' RETURNING count;'''.format( my_word ))
+def increase_a_word_count( cursor, my_word, how_much_to_increase = 1 ):
+    cur.execute('''UPDATE "Tweetwordcount" SET count=count+%s WHERE word = '%s' RETURNING count;''' % (how_much_to_increase, my_word) )
     if not cur.fetchall():   #python idiom for empty list
-       cur.execute('''INSERT INTO tweetwordcount VALUES ( '{}', 1 );'''.format( my_word ))
+       cur.execute('''INSERT INTO "Tweetwordcount" VALUES ( '%s', %s );''' % (my_word, how_much_to_increase) )
+    cursor.connection.commit() #This might be expensive in the long run.
 
 
-increment('dog')
-increment('dog')
-increment('cat')
-increment('zebra')
+increase_a_word_count(cur, 'dog')
+increase_a_word_count(cur, 'dog', 2)
+increase_a_word_count(cur, 'cat', 5)
+increase_a_word_count(cur, 'zebra')
 
-cur.execute("SELECT word, count from tweetwordcount")
+cur.execute('''SELECT word, count from "Tweetwordcount"''')
 records = cur.fetchall()
 for rec in records:
    print "word = ", rec[0]
